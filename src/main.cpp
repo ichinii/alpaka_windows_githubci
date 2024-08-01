1396913ffbe1c500bc7bc475ec7b887ac3ef4f21
Sven Erdem <svenerdem@gmail.com>
2024-08-01 16:11:21 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <alpaka/alpaka.hpp>
+
+using Dim = alpaka::DimInt<1>;
+using Idx = int32_t;
+using Vec = alpaka::Vec<Dim, Idx>;
+
+using AccCpu = alpaka::AccCpuSerial<Dim, Idx>;
+using QueueCpu = alpaka::Queue<AccCpu, alpaka::NonBlocking>;
+
+using AccGpu = alpaka::AccGpuCudaRt<Dim, Idx>;
+using QueueGpu = alpaka::Queue<AccGpu, alpaka::NonBlocking>;
+
+struct Kernel {
+    template <typename TAcc>
+    ALPAKA_FN_ACC
+    void operator() (const TAcc& acc) const {
+        using Idx = alpaka::Idx<TAcc>;
+        const Idx gid = alpaka::getIdx<alpaka::Grid, alpaka::Threads>(acc)[0];
+    }
+};
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+    const auto count = alpaka::getDevCount(alpaka::PlatformCpu());
+    assert(0 < count);
+
+    const auto N = static_cast<Idx>(1 << 10);
+    const auto platform = alpaka::Platform<AccCpu>();
+    const auto acc = alpaka::getDevByIdx(platform, 0);
+    auto d_q = alpaka::Queue<AccCpu, alpaka::Blocking>(acc);
+
+    alpaka::exec<AccCpu>(
+        d_q,
+        alpaka::getValidWorkDiv<AccCpu>(acc, N),
+        Kernel{}
+    );
+
+    alpaka::wait(d_q);
+
+    std::cout << "success" << std::endl;
+
+    return 0;
+}
